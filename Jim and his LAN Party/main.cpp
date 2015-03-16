ff8452cbd444f6ec5980b47d862b5430f25f448b
yeyejyf <yunfan.jin@gmail.com>
2015-03-15 22:43:31 -0400

    n/a

--- Jim and his LAN Party/main.cpp
+++ Jim and his LAN Party/main.cpp
@@ -7,6 +7,43 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+class MultiGroups{
+    long current, N;
+    vector<set<long>> groups;
+    vector<long> PlayerChoice;
+    
+public:
+    MultiGroups(long N):N(N),current(0){
+        PlayerChoice = vector<long>(N+1, -1);
+    }
+    void addToGroup(long player, long GroupNo){
+        PlayerChoice[player] = GroupNo;
+        groups[GroupNo].insert(player);
+    }
+    long newGroup(){
+        long tmp = current;
+        current ++;
+        groups.push_back(set<long>());
+        return tmp;
+    }
+    
+    void mergeGroup(long g1, long g2){
+        if (groups[g1].size() < groups[g2].size()) {
+            mergeGroup(g2, g1);
+            return;
+        }
+        for (long item: groups[g2]){
+            groups[g1].insert(item);
+            PlayerChoice[item] = g1;
+        }
+        groups[g2] = set<long>();
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
